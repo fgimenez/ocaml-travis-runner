39ab101fdee771fb1cc528fa73e7fdcc69f74704
Federico Gimenez <fgimenez@coit.es>
2016-11-11 15:14:23 +0100

    fixed build path

--- travis-runner.sh
+++ travis-runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/ubuntu || exit
+cd /build || exit
 wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-opam.sh
-su -l -c "cd /home/ubuntu && TRAVIS_OS_NAME=linux OCAML_VERSION=$OCAML_VERSION PACKAGE=$PACKAGE bash -ex .travis-opam.sh" travis
+su -l -c "cd /build && TRAVIS_OS_NAME=linux OCAML_VERSION=$OCAML_VERSION PACKAGE=$PACKAGE bash -ex .travis-opam.sh" travis
